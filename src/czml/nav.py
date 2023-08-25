9fdb6c6e3606fc517021145efe44a674963ec802
Sanjog <st0128@uah.edu>
2023-08-25 11:07:56 -0500

    changes for data ingest

--- src/czml/nav.py
+++ src/czml/nav.py
@@ -1,31 +1,33 @@
-from typing import string, Array
 import boto3
 from copy import deepcopy
 import numpy as np
 import pandas as pd
+from typing import Generator
+
 from abstract.czml_data_process import CZMLDataProcess
 from utils.czml_writer_nav import NavCzmlWriter
 
 class NavCZMLDataProcess(CZMLDataProcess):
-  def ingest(self, url: string) -> Array[np.array]:
-    super().ingest()
+  def __init__(self):    
+    pass
+  
+  def ingest(self, url: str) -> np.array:
     s3_client = boto3.client('s3')
     [bucket_name, objectKey] = self._get_s3_details(url)
     s3_file = s3_client.get_object(Bucket=bucket_name, Key=objectKey)
 
     file = s3_file['Body'].iter_lines()
-    data = np.loadtxt(file, delimiter=',', converters=self.converters)
+    
+    data = self._generator_to_np(file)
     return data
   
-  def preprocess(self, data: Array[np.array]) -> pd.DataFrame:
-    super().preprocess()
+  def preprocess(self, data: np.array) -> pd.DataFrame:
     cleaned_data = self._cleaning(data)
     transformed_data = self._transformation(cleaned_data)
     integrated_data = self._integration(transformed_data)
     return integrated_data
 
-  def prep_visualization(self, data: pd.DataFrame) -> string:
-    super().prep_visualization()
+  def prep_visualization(self, data: pd.DataFrame) -> str:
     nav_czml_writer = NavCzmlWriter(data["length"], data["time_window"], data["time_steps"], data["longitude"], data["latitude"], data["altitude"], data["roll"], data["pitch"], data["heading"])
     nav_czml_str = nav_czml_writer.get_czml_string()
     return nav_czml_str
@@ -33,7 +35,7 @@ class NavCZMLDataProcess(CZMLDataProcess):
 
   # data preprocessing steps
 
-  def _cleaning(self, data: Array[np.array]) -> Array[np.array]:
+  def _cleaning(self, data: np.array) -> np.array:
     col_index_map = self._get_col_index_map()
     data = deepcopy(data)
 
@@ -79,24 +81,26 @@ class NavCZMLDataProcess(CZMLDataProcess):
     
     return np.hstack((f_time_window, f_time_steps, f_latitude, f_longitude, f_altitude, f_heading, f_pitch, f_roll, f_length))
   
-  def _transformation(self, data: Array[np.array]) -> Array[np.array]:
+  def _transformation(self, data: np.array) -> np.array:
     #  no transformation needed
     return data
   
-  def _integration(self, data: Array[np.array]) -> pd.DataFrame:
+  def _integration(self, data: np.array) -> pd.DataFrame:
     # use data and create a dataframe
     return pd.DataFrame(data=data, columns= ["time_window", "time_steps", "latitude", "longitude", "altitude", "heading", "pitch", "roll", "length"])
 
   # utils
 
-  def _get_s3_details(url):
+  def _get_s3_details(self, url) -> list:
     url = url.replace("s3://", "")
-    url = url.split("/")
-    bucket_name = url[0]
-    objectKey = url[1]
+    temp_url = url.split("/")
+    bucket_name = temp_url[0]
+    
+    temp_url = url.split(bucket_name+"/")
+    objectKey = temp_url[1]  # key should not start with /
     return [bucket_name, objectKey]
   
-  def _get_col_index_map():
+  def _get_col_index_map(self):
     return {
         "time": 1,
         "latitude": 2,
@@ -106,3 +110,32 @@ class NavCZMLDataProcess(CZMLDataProcess):
         "pitch": 16,
         "roll": 17
     }
+    
+  def _generator_to_np(self, infile: Generator) -> np.array:
+    # As the data in txt is all string, to put it inside numpy array, we need to convert it to appropirate types
+    
+    # create null converters
+    converters = {}
+    for i in range(33): # 33 cols/feature data
+      converters[i] = lambda _: np.nan 
+    
+    # upadate converters for appropriate faeture/cols with appropriate functions
+    col_index_map = self._get_col_index_map()
+    converters[col_index_map["time"]] = lambda x: np.datetime64(x, 's').astype(np.int64)
+    converters[col_index_map["latitude"]] = self._string_to_float
+    converters[col_index_map["longitude"]] = self._string_to_float
+    converters[col_index_map["altitude"]] = self._string_to_float
+    converters[col_index_map["heading"]] = self._string_to_float
+    converters[col_index_map["pitch"]] = self._string_to_float
+    converters[col_index_map["roll"]] = self._string_to_float
+    
+    # apply converter during txt load
+    return np.loadtxt(infile, delimiter=',', converters=converters)
+  
+  def _string_to_float(self, str: str) -> np.float64:
+        value = np.nan
+        try:
+            value = float(str)
+        except:
+            pass
+        return value
