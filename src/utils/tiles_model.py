10e9480a01498eee7e1a54711a030eba07015d9c
Sanjog <st0128@uah.edu>
2023-08-29 10:52:21 -0500

    add pointcloud generation utilities

---
+++ src/utils/tiles_model.py
@@ -0,0 +1,18 @@
+tileset_json = {
+	"asset": {
+		"version": "1.0",
+		"type": "Airborne Radar"
+	},
+	"root": {
+		"geometricError": 1000000,
+		"refine" : "REPLACE",
+		"boundingVolume": {
+            "region": []
+        },
+        "children": []
+	},
+    "properties": {
+        "epoch": "",
+        "refined": []
+    }
+}
