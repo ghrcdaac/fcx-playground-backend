affb2624beb19cb4caeac413f60949279939f34b
Sanjog <st0128@uah.edu>
2023-08-29 11:33:38 -0500

    updated:: details on czml data process abstract class

--- src/abstract/czml_data_process.py
+++ src/abstract/czml_data_process.py
@@ -7,15 +7,16 @@ from .data_process import DataProcess
 class CZMLDataProcess(DataProcess):
   @abstractmethod
   def ingest(self, url: str) -> np.array:
-    print("get czml data from data source. Data source may be any of http or ftp")
-    # czml data can be in any file format. eg. txt, nc, uf etc.
+    print("get raw data from data source. Data source may be any of http or ftp")
+    # raw data can be in any file format. eg. txt, nc, uf etc.
     # use appropriate library to read the file and return the data as numpy arrays
   
   @abstractmethod
   def preprocess(self) -> pd.DataFrame:
-    print("preprocess czml data to be ready for vizualization")
-    # pre process of czml can go through various stages like
-    # cleaning: scrape the necessary data columns, 
+    print("preprocess raw data to be ready for vizualization")
+    # pre process of raw data can go through various stages like
+    # cleaning: scrape the necessary data columns,
+    # transformation: transform the data to a suitable data formatting
     # integration: data from multiple sources can be integrated into intermediate format, e.g. numpy arrays. The intermeidate format should be compatible with viz prepration step
     
   @abstractmethod
